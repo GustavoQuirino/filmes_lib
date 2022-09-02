e12dc60a21ef2efbff9548c80eab2427fa118f4e
GustavoQuirino <gh.quirino@yahoo.com>
2022-09-02 11:22:49 -0300

    detalhes

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -5,6 +5,7 @@ import { BsGraphUp, BsWallet2,BsHourglassSplit, BsFillFileEarmarkTextFill} from 
 import MoviesCard from "../components/MoviesCard"
 import "./Movie.css"
 
+
 const moviesUrl = 'https://api.themoviedb.org/3/movie/'
 const key = '?api_key=7bbc44e17661121420501e26335e6341'
 
@@ -21,14 +22,20 @@ const Movie = ()=>{
     useEffect(()=> {
         const movieUrl = `${moviesUrl}${id}${key}&language=pt-BR`
         getMovie(movieUrl)
-        console.log(movieUrl)
+        console.log(movie)
     },[ ])
 
     return (
-        <>
-            <h1>Detalhes</h1>
+        <div className="detalhes">
             {movie && <MoviesCard movie={movie}/>}
-        </>
+            {movie && 
+            <div>
+                <h2>{movie.title}</h2>
+                <h2>Ano de Lancamento : {movie.release_date}</h2>
+                <p>Sinopse: {movie.overview}</p> 
+            </div>
+            }
+        </div>
     )
 }
 
